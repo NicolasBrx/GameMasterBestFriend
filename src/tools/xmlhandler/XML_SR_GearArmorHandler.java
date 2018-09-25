c9287f448653132f7972c0d8d5fa7bfdf3b93312
Nicolas Brax <nicolas@brax.ovh>
2018-09-25 12:46:25 +0200

    WIP  - XML skeleton addition;  - Improved JavaDoc.

---
+++ src/tools/xmlhandler/XML_SR_GearArmorHandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools.xmlhandler;
+
+/**
+ *
+ * @author nibrax
+ */
+public class XML_SR_GearArmorHandler {
+  
+}
